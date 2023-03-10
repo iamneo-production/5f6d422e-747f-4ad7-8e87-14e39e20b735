c7734eb99d7e373b55f73cf915d8042495b1e77c
MihikaSrivastava-14 <makkhushi1@gmail.com>
2023-03-10 06:52:20 +0000

    com

---
+++ sidebar-01/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.get("/index2.html", function(req, res){
+    res.sendFile(__dirname + "/index2.html")
+    // res.fetch(__dirname + "/index2.html")
+});
+
+app.get("/meetings.html", function(req, res){
+    res.sendFile(__dirname + "/meetings.html")
+});
+
+app.get("/mentors.html", function(req, res){
+    res.sendFile(__dirname + "/mentors.html")
+});
+
+app.listen(8081, ()=>{
+    console.log("Server is running");
+});
